fa66ce0fbd2f66456778285ebc42b69f575af292
perninha <carlos.aquila@hotmail.com>
2024-08-25 04:48:11 -0300

    :sparkles: Adiciona navbar

---
+++ portfolio-website/app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="flex flex-wrap items-center justify-between mx-auto px-16 py-4">
+        <a href="/" className="text-1xl text-black font-semibold">
+        {/* Imagem do logo */}
+          <Image src="/images/logominiblack.svg" alt="Logo"  width={50} height={50} />
+        </a>
+        <div className="menu"></div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- portfolio-website/app/page.tsx
+++ portfolio-website/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
 import HeroSection2 from "./components/HeroSection2";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-white overflow-hidden">
+      <Navbar />
       <HeroSection />
     </main>
   );
