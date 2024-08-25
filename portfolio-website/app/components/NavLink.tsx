c4bfaa21e3af28ccd29a1c3b23d7e2ff15fca043
perninha <carlos.aquila@hotmail.com>
2024-08-25 18:35:58 -0300

    :sparkles: Altera cor dos botões do NavBar

--- portfolio-website/app/components/NavLink.tsx
+++ portfolio-website/app/components/NavLink.tsx
@@ -4,7 +4,7 @@ const NavLink = ({ href, title }: { href: string, title: string }) => {
   return (
     <Link
       href={href}
-      className="block py-2 pl-3 pr-4 text-black sm:text-xl rounded md:p-0 hover:text-gray-700"
+      className="block py-2 pl-3 pr-4 text-black sm:text-xl rounded md:p-0 hover:text-gray-500"
     >
       {title}
     </Link>
