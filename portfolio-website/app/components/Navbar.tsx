b656c8a8e444db3dfd5311384af1321f8303ffde
perninha <carlos.aquila@hotmail.com>
2024-08-25 18:36:24 -0300

    :sparkles: Adiciona blur e fixa o menu no topo

--- portfolio-website/app/components/Navbar.tsx
+++ portfolio-website/app/components/Navbar.tsx
@@ -24,7 +24,7 @@ const NavLinks = [
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-white bg-opacity-40 backdrop-blur-sm">
       <div className="flex flex-wrap items-center justify-between mx-auto px-3 py-3 sm:px-16 sm:py-4">
         <Link href={"/"} className="text-1xl text-black font-semibold">
         {/* Imagem do logo */}
