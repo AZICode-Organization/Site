d248a38eae5bf25851d7a0e92fccd6fdefce14b7
perninha <carlos.aquila@hotmail.com>
2024-08-25 20:41:34 -0300

    :sparkles: Inicia a criação do aboutSection

--- portfolio-website/app/page.tsx
+++ portfolio-website/app/page.tsx
@@ -1,12 +1,15 @@
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import AboutSection from "./components/AboutSection";
 import HeroSection2 from "./components/HeroSection2";
+
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-white overflow-hidden">
       <Navbar />
       <HeroSection />
+      <AboutSection />
     </main>
   );
 }
