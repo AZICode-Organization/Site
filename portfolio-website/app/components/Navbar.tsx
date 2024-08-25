d2ba84a40ffb857af9da597804e19918c1895e8f
perninha <carlos.aquila@hotmail.com>
2024-08-25 07:09:12 -0300

    :sparkles: Adiciona itens do navbar

--- portfolio-website/app/components/Navbar.tsx
+++ portfolio-website/app/components/Navbar.tsx
@@ -1,15 +1,46 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
+import NavLink from './NavLink'
+
+const NavLinks = [
+  {
+    title: 'Sobre nós',
+    href: '#sobre'
+  },
+  {
+    title: 'Serviços',
+    href: '#servicos'
+  },
+  {
+    title: 'Portfólio',
+    href: '#portfolio'
+  },
+  {
+    title: 'contato',
+    href: '#contato'
+  },
+]
 
 const Navbar = () => {
   return (
     <nav>
       <div className="flex flex-wrap items-center justify-between mx-auto px-3 py-3 sm:px-16 sm:py-4">
-        <a href="/" className="text-1xl text-black font-semibold">
+        <Link href={"/"} className="text-1xl text-black font-semibold">
         {/* Imagem do logo */}
           <Image src="/images/logominiblack.svg" alt="Logo"  width={50} height={50} />
-        </a>
-        <div className="menu"></div>
+        </Link>
+        <div className="menu block md:w-auto" id="navbar">
+          <ul>
+            {
+              NavLinks.map((link, index) => (
+                <li key={index} className="inline-block px-3 py-3">
+                  <NavLink href={link.href} title={link.title} />
+                </li>
+              ))
+            }
+          </ul>
+        </div>
       </div>
     </nav>
   )
