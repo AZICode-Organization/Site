fc83a4f0edeefd903e1b9a71fb6085dd4ed19d5b
perninha <carlos.aquila@hotmail.com>
2024-08-25 05:05:25 -0300

    :sparkles: Adiciona design do mobile

--- portfolio-website/app/components/Navbar.tsx
+++ portfolio-website/app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Navbar = () => {
   return (
     <nav>
-      <div className="flex flex-wrap items-center justify-between mx-auto px-16 py-4">
+      <div className="flex flex-wrap items-center justify-between mx-auto px-3 py-3 sm:px-16 sm:py-4">
         <a href="/" className="text-1xl text-black font-semibold">
         {/* Imagem do logo */}
           <Image src="/images/logominiblack.svg" alt="Logo"  width={50} height={50} />
