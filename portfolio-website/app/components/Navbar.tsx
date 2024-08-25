7167fd8c7e42d35abe8bf976cfdbe2523dd7abd3
perninha <carlos.aquila@hotmail.com>
2024-08-25 20:40:22 -0300

    :sparkles: Adiciona menu navbar mobile

--- portfolio-website/app/components/Navbar.tsx
+++ portfolio-website/app/components/Navbar.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import NavLink from './NavLink'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/16/solid';
+import MenuOverlay from './MenuOverlay';
 
 const NavLinks = [
   {
@@ -23,14 +26,34 @@ const NavLinks = [
 ]
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-white bg-opacity-40 backdrop-blur-sm">
       <div className="flex flex-wrap items-center justify-between mx-auto px-3 py-3 sm:px-16 sm:py-4">
+
         <Link href={"/"} className="text-1xl text-black font-semibold">
-        {/* Imagem do logo */}
-          <Image src="/images/logominiblack.svg" alt="Logo"  width={50} height={50} />
+          {/* Imagem do logo */}
+          <Image src="/images/logominiblack.svg" alt="Logo" width={50} height={50} />
         </Link>
-        <div className="menu block md:w-auto" id="navbar">
+
+        {/* Botão de menu */}
+        <div className="mobile-menu block md:hidden">
+          {
+            !isOpen ? (
+              <button onClick={() => setIsOpen(true)} className="flex items-center px-3 py-2 text-gray-700 hover:text-black hover:border-black">
+                <Bars3Icon clasName="h-5 w-5" width={22} height={22}/>
+              </button>
+            ) : (
+              <button onClick={() => setIsOpen(false)} className="flex items-center px-3 py-2 text-gray-700 hover:text-white hover:border-white">
+                <XMarkIcon clasName="h-5 w-5" width={22} height={22}/>
+              </button>
+            )
+          }
+        </div>
+
+
+        <div className="menu hidden md:block md:w-auto" id="navbar">
           <ul>
             {
               NavLinks.map((link, index) => (
@@ -42,6 +65,7 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
+      {isOpen? <MenuOverlay links={NavLinks} /> : null}
     </nav>
   )
 }
