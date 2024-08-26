825f2fc1b04129b3e5d0bfee78509f48607bbe00
perninha <carlos.aquila@hotmail.com>
2024-08-25 21:45:35 -0300

    :heavy_plus_sign: Adiciona dependencias

--- portfolio-website/app/components/AboutSection.tsx
+++ portfolio-website/app/components/AboutSection.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AboutSection = () => {
   return (
     <section className="text-black">
-      <div className="md:grid md:grid-cols-2 gap-8 items-center">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-u px-4 xl:gap-16 sm:py-16 xl:px-16">
         <h1 className="text-4xl font-bold">About Me</h1>
       </div>
       AboutSection
