9a0ced35490a9a1e9052a0324f3780eaec5575d3
perninha <carlos.aquila@hotmail.com>
2024-08-26 17:46:46 -0300

    :sparkles: Adiciona animação nos itens do navbar

--- portfolio-website/app/components/NavLink.tsx
+++ portfolio-website/app/components/NavLink.tsx
@@ -4,7 +4,7 @@ const NavLink = ({ href, title }: { href: string, title: string }) => {
   return (
     <Link
       href={href}
-      className="block py-2 pl-3 pr-4 text-black sm:text-xl rounded md:p-0 hover:text-gray-500"
+      className="block py-2 pl-3 pr-4 font-light text-black sm:text-xl rounded md:p-0 hover:transform hover:scale-105 transition-transform"
     >
       {title}
     </Link>
