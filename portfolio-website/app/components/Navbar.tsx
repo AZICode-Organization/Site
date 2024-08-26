62b12e2427d5d115d27ce183d45dfe15116d80d7
perninha <carlos.aquila@hotmail.com>
2024-08-25 22:22:04 -0300

    :sparkles: Reduz o tamanho do navbar

--- portfolio-website/app/components/Navbar.tsx
+++ portfolio-website/app/components/Navbar.tsx
@@ -30,7 +30,7 @@ const Navbar = () => {
 
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-white bg-opacity-40 backdrop-blur-sm">
-      <div className="flex flex-wrap items-center justify-between mx-auto px-3 py-3 sm:px-16 sm:py-4">
+      <div className="flex flex-wrap items-center justify-between mx-auto px-3 py-3 sm:px-16 sm:py-2">
 
         {/* Logo */}
         <Link href={"/"} className="text-1xl text-black font-semibold">
