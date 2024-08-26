86563a4d5d0e7fefbb5be0073ed69bcc3acfc3f0
perninha <carlos.aquila@hotmail.com>
2024-08-26 01:11:05 -0300

    :sparkles: Finaliza criação basica do about

--- portfolio-website/app/components/AboutSection.tsx
+++ portfolio-website/app/components/AboutSection.tsx
@@ -8,7 +8,7 @@ const AboutSection = () => {
       <div className="md:grid md:grid-cols-12 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-20 xl:px-16">
 
         {/* Coluna 1 - Título */}
-        <div className="col-span-12 md:col-span-3 mb-4 md:mb-0 self-start">
+        <div className="col-span-12 md:col-span-3 mb-10 md:mb-0 self-start pl-6 md:pl-0 mt-10 md:mt-0">
           <div>
             <h2 className="text-xl font-light">
               Sobre nós
@@ -20,18 +20,18 @@ const AboutSection = () => {
         </div>
 
         {/* Coluna 2 - Texto Principal */}
-        <div className="col-span-12 md:col-span-7 mb-4 md:mb-0">
-          <p className="text-2xl font-extralight">
+        <div className="col-span-12 md:col-span-7 mb-4 md:mb-0 pr-4 md:pr-0">
+
+          <p className="text-2xl font-light">
             A AZI CODE revoluciona a gestão empresarial com <span className="font-semibold text-indigo-900">soluções tecnológicas inovadoras </span>
             e <span className="font-semibold text-indigo-900">desenvolvimento de software sob medida.</span>
           </p>
-          <p className="mt-3">
-            Combinamos expertise em tecnologia com um profundo entendimento das necessidades de empresários locais, garantindo
-            resultados que fazem a diferença.
+          <p className="mt-3 md:hidden mb-6 text-xl">
+            Projetamos as soluções digitais que impulsionam o futuro da sua empresa.
           </p>
 
           {/* Botões */}
-          <div className="mt-3">
+          <div className="mt-3 text-right md:text-left mb-9 md:mb-0">
 
             <button className="px-6 py-2 w-fit sm:w-fit rounded-full bg-transparent hover:bg-slate-200 text-black border border-gray-800 mt-3">
               Conheça a equipe
