f08b6f403202902026b8624701e7ace617b5bef2
perninha <carlos.aquila@hotmail.com>
2024-08-27 03:51:42 -0300

    :sparkles: Adiciona botões de filtros de projeto

--- portfolio-website/app/components/ProjectSection.tsx
+++ portfolio-website/app/components/ProjectSection.tsx
@@ -4,48 +4,48 @@ import ProjectCard from './ProjectCard'
 const projectsData = [
   {
     id: 1,
-    title: "Projeto 1",
-    description: "Descrição do projeto 1",
+    title: "Solara Express",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo neque in libero auctor hendrerit.",
     image: "/images/projects/ProjetoSolara.png",
     tag: ["All", "Web"],
     previewUrl: "https://www.google.com"
   },
   {
     id: 2,
-    title: "Projeto 2",
-    description: "Descrição do projeto 2",
+    title: "Sobrenatural",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo neque in libero auctor hendrerit.",
     image: "/images/projects/ProjetoWhite.png",
     tag: ["All", "Web"],
     previewUrl: "https://www.google.com"
   },
   {
     id: 3,
-    title: "Projeto 3",
-    description: "Descrição do projeto 3",
+    title: "O caminho até aqui",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo neque in libero auctor hendrerit.",
     image: "/images/projects/ProjetoColorFull.png",
     tag: ["All", "Web"],
     previewUrl: "https://www.google.com"
   },
   {
     id: 4,
-    title: "Projeto 4",
-    description: "Descrição do projeto 4",
+    title: "Início de tudo",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo neque in libero auctor hendrerit.",
     image: "/images/projects/projeto.png",
     tag: ["All", "Web"],
     previewUrl: "https://www.google.com"
   },
   {
     id: 5,
-    title: "Projeto 5",
-    description: "Descrição do projeto 5",
+    title: "George Carlin",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo neque in libero auctor hendrerit.",
     image: "/images/projects/ProjectMaster.png",
     tag: ["All", "Web"],
     previewUrl: "https://www.google.com"
   },
   {
     id: 6,
-    title: "Projeto 6",
-    description: "Descrição do projeto 6",
+    title: "Expresso Nepomuceno",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo neque in libero auctor hendrerit.",
     image: "/images/projects/Projeto2colors.png",
     tag: ["All", "Web"],
     previewUrl: "https://www.google.com"
@@ -56,7 +56,19 @@ const projectsData = [
 const ProjectSection = () => {
   return (
     <section className="bg-neutral-950 px-16">
-      <h2 className="text-center text-4xl font-bold text-white mt-16 mb-8 md:mb-12">Uma pequena seleção de <span className="text-indigo-700">projetos recentes</span></h2>
+      <h2 className="text-center text-4xl font-bold text-white mt-16 mb-8 md:mb-12">
+        Uma pequena seleção de <span className="text-indigo-700">projetos recentes</span>
+      </h2>
+
+      <div className="text-white flex flex-row justify-center items center gap-2 pb-12">
+        <button className="rounded-xl bg-indigo-700  px-3 py-1">Todos</button>
+        <button className="rounded-xl bg-white text-black px-3 py-1 hover:bg-indigo-700 hover:text-white">Chat Bots</button>
+        <button className="rounded-xl bg-white text-black px-3 py-1 hover:bg-indigo-700 hover:text-white">Softwares</button>
+        <button className="rounded-xl bg-white text-black px-3 py-1 hover:bg-indigo-700 hover:text-white">Web Sites</button>
+        <button className="rounded-xl bg-white text-black px-3 py-1 hover:bg-indigo-700 hover:text-white">UI/UX</button>
+        <button className="rounded-xl bg-white text-black px-3 py-1 hover:bg-indigo-700 hover:text-white">SEO</button>
+      </div>
+
       <div className="grid md:grid-cols-3 gap-8 md:gap-12">
         {projectsData.map((project) =>
           <ProjectCard
